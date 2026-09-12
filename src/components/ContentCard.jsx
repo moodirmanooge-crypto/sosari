bb5a8be9c067e53977c2b1e7858e1cb460be31cc
moodir manooge <moodirmanooge@gmail.com>
2026-09-12 17:21:53 +0300

    Update project

--- src/components/ContentCard.jsx
+++ src/components/ContentCard.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { ImageReveal } from "./Motion";
+import { IconArrow } from "./Icons";
 
 export default function ContentCard({ item }) {
   return (
@@ -10,6 +11,7 @@ export default function ContentCard({ item }) {
       {item.tag && <span className="tag">{item.tag}</span>}
       <h3>{item.title}</h3>
       {item.summary && <p>{item.summary}</p>}
+      <span className="cardReadMore">Read More <IconArrow /></span>
     </Link>
   );
 }
